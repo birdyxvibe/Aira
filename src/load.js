ef534295bebfe2bc97f911da3f0933fe31fb1070
birdyxvibe <birdythebirb2@gmail.com>
2025-04-23 22:52:46 -0500

    refactor(git): updated gitignore to now include eslint config and load.js
    IMPORTANT: UPDATE ENV FILE TO CONTAIN CLIENT
    AND (if local) GUILD IDS!!!!!

---
+++ src/load.js
@@ -0,0 +1,34 @@
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v10';
+import data from './commands.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const clientId = process.env.clientID;
+const guildId = process.env.guildID ?? null;
+
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+
+(async () => {
+	try {
+		console.log(`📝 Refreshing ${data.length} commands...`);
+
+		let cmds;
+		if (guildId) {
+			cmds = await rest.put(
+				Routes.applicationGuildCommands(clientId, guildId),
+				{ body: data }
+			);
+		} else {
+			cmds = await rest.put(
+				Routes.applicationCommands(clientId),
+				{ body: data }
+			);
+		}
+
+		console.log(`📝 Refreshed ${cmds.length} ${guildId ? 'GUILD' : 'CLIENT'} commands!`);
+	} catch (err) {
+		console.error(err);
+		console.log('⚠️ Oops! Something went wrong, please try again!');
+	}
+})();
