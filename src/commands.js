e2f04dcbbc931e185c23b4d3fbb338d1a9ba2262
birdyxvibe <birdythebirb2@gmail.com>
2025-04-23 22:47:05 -0500

    refactor: updated commands to js import (from json)

---
+++ src/commands.js
@@ -0,0 +1,10 @@
+export default [
+	{
+		'name': 'ping',
+		'description': 'Check the application\'s latency.'
+	},
+	{
+		'name': 'help',
+		'description': 'View a list of all application commands.'
+	}
+];
